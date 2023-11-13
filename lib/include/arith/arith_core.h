a7de19498497ade7b3aaf1c53673303353f4b016
PleYa <aaiaia.gg@gmail.com>
2023-11-14 01:05:24 +0900

    [🚧] added mul_u32()

--- lib/include/arith/arith_core.h
+++ lib/include/arith/arith_core.h
@@ -1,12 +1,14 @@
-#ifndef ADD_H
-#define ADD_H
+#ifndef ARITH_CORE_H
+#define ARITH_CORE_H
 
 #include <stdlib.h>
 
+#include "common/returnType.h"
 #include "common/ntype.h"
 #include "common/util.h"
 
 NTYPE add_u32(ntype_s* d, ntype_s* s1, ntype_s* s0, NTYPE c);
 NTYPE sub_u32(ntype_s* d, ntype_s* s1, ntype_s* s0, NTYPE c);
+ReturnType mul_u31(ntype_s* d, ntype_s* s1, ntype_s* s0);
 
 #endif
--- lib/src/arith/arith_core.c
+++ lib/src/arith/arith_core.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
-#include "arith/core.h"
+#include "arith/arith_core.h"
 
 NTYPE add_u32(ntype_s* d, ntype_s* s1, ntype_s* s0, NTYPE c) {
     for(size_t i=0ul; i<d->length; i++) {
@@ -25,4 +26,22 @@ NTYPE sub_u32(ntype_s* d, ntype_s* s1, ntype_s* s0, NTYPE c) {
         d->data[i] = s;
     }
     return c;
+}
+
+ReturnType mul_u31(ntype_s* d, ntype_s* s1, ntype_s* s0) {
+    if(((d->length) >= ((s1->length)<<1U)) && ((d->length) >= ((s0->length)<<1U))) {
+        // clear destination 'd'
+        (void)memset(d->data, 0x0U, d->size);
+
+        ntype_s* tmp = mkNum(d->bits);
+        // clear temp 'tmp'
+        (void)memset((tmp->data + s0->length), 0x0U, (tmp->size - s0->size));
+        (void)memcpy(tmp->data, s0->data, s0->size);
+
+        for(size_t bShft = d->bits; bShft != 0U; bShft--) {
+            /* INSERT STATEMENTS */
+        }
+    } else {
+        return E_ERROR_ARGS;
+    }
 }
