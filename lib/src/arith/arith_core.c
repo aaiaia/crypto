b096a7844173751efd0f0e9e570eb3031a1863ce
PleYa <aaiaia.gg@gmail.com>
2023-11-21 01:48:54 +0900

    added NULL pointer exception in mul_u32_bs_ext()

--- lib/src/arith/arith_core.c
+++ lib/src/arith/arith_core.c
@@ -33,33 +33,38 @@ NTYPE sub_u32(ntype_s* d, ntype_s* s1, ntype_s* s0, NTYPE c) {
 // s0 accumulates then shift left
 // s1 checks inclease data index and shift likes bit witth
 ReturnType mul_u32_bs_ext(ntype_s* d, ntype_s* s1, ntype_s* s0, bool guard) {
-    if((d->length) >= (s1->length + s0->length) || (!guard)) {
-        // clear destination 'd'
-        (void)memset(d->data, 0x0U, d->size);
+    if((d != NULL) && (s1 != NULL) && (s0 != NULL)) {
+        if((d->length) >= (s1->length + s0->length) || (!guard)) {
+            // clear destination 'd'
+            (void)memset(d->data, 0x0U, d->size);
 
-        ntype_s* tmp = mkNum(d->bits);
+            ntype_s* tmp = mkNum(d->bits);
 
-        // clear temp 'tmp'
-        (void)memset((tmp->data + s0->length), 0x0U, (tmp->size - s0->size));
-        (void)memcpy(tmp->data, s0->data, s0->size);
+            // clear temp 'tmp'
+            (void)memset((tmp->data + s0->length), 0x0U, (tmp->size - s0->size));
+            (void)memcpy(tmp->data, s0->data, s0->size);
 
-        size_t nSftBit = s0->bits;
-        for(size_t i = 0U; i < s1->length; i++) {
-            size_t sftBit = (nSftBit >= NTYPE_BITS)?(NTYPE_BITS):(nSftBit);
-            for(size_t sft = 0U; sft < sftBit; sft++) {
-                if(((s1->data[i] >> sft) & 0x1U) != 0x0u) {
-                    add_u32(d, d, tmp, 0U);
-                } else { /* Do nothing */}
-                sftL1b(tmp, NULL, 0U);
+            size_t nSftBit = s0->bits;
+            for(size_t i = 0U; i < s1->length; i++) {
+                size_t sftBit = (nSftBit >= NTYPE_BITS)?(NTYPE_BITS):(nSftBit);
+                for(size_t sft = 0U; sft < sftBit; sft++) {
+                    if(((s1->data[i] >> sft) & 0x1U) != 0x0u) {
+                        add_u32(d, d, tmp, 0U);
+                    } else { /* Do nothing */}
+                    sftL1b(tmp, NULL, 0U);
+                }
+                nSftBit-=sftBit;
             }
-            nSftBit-=sftBit;
-        }
-        rmNum(&tmp);
+            rmNum(&tmp);
 
-        if(nSftBit != 0U) {
-            return E_ERROR_RUNTIME;
-        } else { /* Do nothing */ }
+            if(nSftBit != 0U) {
+                return E_ERROR_RUNTIME;
+            } else { /* Do nothing */ }
+        } else {
+            return E_ERROR_ARGS;
+        }
     } else {
-        return E_ERROR_ARGS;
+        return E_ERROR_NULL;
     }
+    return E_OK;
 }
