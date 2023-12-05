f784a4f597b79f3377d12efcb2b314f862a5f2ed
PleYa <aaiaia.gg@gmail.com>
2023-12-06 00:39:37 +0900

    added MACRO BIT_U8_SIZE(), BIT_U16_SIZE(), BIT_U32_SIZE() and BIT_U64_SIZE()

--- lib/include/common/util.h
+++ lib/include/common/util.h
@@ -5,7 +5,10 @@
 #define INT_CEIL(n, m)  ((n/m)+(((n%m)!=0)?(1):(0)))
 
 #define LASTBITMASK(bits, TYPE) ((TYPE)(-1)>>(sizeof(TYPE)<<3UL)-(bits%(sizeof(TYPE)<<3UL)))
-#define BIT_SIZE(bits)  ((bits>>3u)+(((bits&0x7u)!=0x0u)?(1u):(0u)))
+#define BIT_U8_SIZE(bits)   ((bits>>3u)+(((bits&0x07u)!=0x0u)?(1u):(0u)))
+#define BIT_U16_SIZE(bits)  ((bits>>3u)+(((bits&0x0Fu)!=0x0u)?(1u):(0u)))
+#define BIT_U32_SIZE(bits)  ((bits>>3u)+(((bits&0x1Fu)!=0x0u)?(1u):(0u)))
+#define BIT_U64_SIZE(bits)  ((bits>>6u)+(((bits&0x3Fu)!=0x0u)?(1u):(0u)))
 #define U16_SIZE(size)  ((size&(~0x1U))+(((size & 0x1U)!=0U)?(2U):(0U)))
 #define U32_SIZE(size)  ((size&(~0x3U))+(((size & 0x3U)!=0U)?(4U):(0U)))
 #define U64_SIZE(size)  ((size&(~0x7U))+(((size & 0x7U)!=0U)?(8U):(0U)))
