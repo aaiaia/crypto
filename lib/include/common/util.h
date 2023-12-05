efcc484f8bce4a8fde4f561025a22cfc45a27a1a
PleYa <aaiaia.gg@gmail.com>
2023-12-06 00:53:46 +0900

    fixed critical typo, BIT_U16_SIZE(), BIT_U32_SIZE()

--- lib/include/common/util.h
+++ lib/include/common/util.h
@@ -6,8 +6,8 @@
 
 #define LASTBITMASK(bits, TYPE) ((TYPE)(-1)>>(sizeof(TYPE)<<3UL)-(bits%(sizeof(TYPE)<<3UL)))
 #define BIT_U8_SIZE(bits)   ((bits>>3u)+(((bits&0x07u)!=0x0u)?(1u):(0u)))
-#define BIT_U16_SIZE(bits)  ((bits>>3u)+(((bits&0x0Fu)!=0x0u)?(1u):(0u)))
-#define BIT_U32_SIZE(bits)  ((bits>>3u)+(((bits&0x1Fu)!=0x0u)?(1u):(0u)))
+#define BIT_U16_SIZE(bits)  ((bits>>4u)+(((bits&0x0Fu)!=0x0u)?(1u):(0u)))
+#define BIT_U32_SIZE(bits)  ((bits>>5u)+(((bits&0x1Fu)!=0x0u)?(1u):(0u)))
 #define BIT_U64_SIZE(bits)  ((bits>>6u)+(((bits&0x3Fu)!=0x0u)?(1u):(0u)))
 #define U16_SIZE(size)  ((size&(~0x1U))+(((size & 0x1U)!=0U)?(2U):(0U)))
 #define U32_SIZE(size)  ((size&(~0x3U))+(((size & 0x3U)!=0U)?(4U):(0U)))
